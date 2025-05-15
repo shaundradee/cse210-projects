fb929bd1135b9d05fb5e547528c3a54b90bbfa7b
shaundradee <shaundra.d.strong@gmail.com>
2025-05-15 11:32:47 -0400

    submitting exercise progress so not to lose it.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "3form";
+        job1._jobTitle = "Sales Coordinator";
+        job1._startYear = 2014;
+        job1._endYear = 2017;
+        job1.Display();
+
+        Job job2 = new Job();
+
+        job2._company = "Lowe's";
+        job2._jobTitle = "Kitchen and Bath Designer";
+        job2._startYear = 2006;
+        job2._endYear = 2012;
+        job2.Display();
+        
+
+
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Resume
+{
+    public string _name; 
+
+    public List<Job> _jobs = new List<Job>;
+
+    public void Display()
+    {
+        
+    }
+
+}
