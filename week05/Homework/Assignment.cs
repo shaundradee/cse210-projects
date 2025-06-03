a605f073a5867ea046f43e0829c912d693417e45
shaundradee <shaundra.d.strong@gmail.com>
2025-06-03 16:28:23 -0400

    fixed protected to private on assignment class attribute

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -3,7 +3,7 @@ using System.Dynamic;
 
 public class Assignment
 {
-    protected string _studentName;
+    private string _studentName;
     private string _topic;
 
     public Assignment(string studentName, string topic)
